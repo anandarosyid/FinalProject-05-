5a2c19788946632b4f6a8c1ed2bce8c500b23227
Abrar Rayhan Muhammad <abrarrayhanmuhammad@gmail.com>
2025-10-02 11:52:22 +0700

    Menambah baris ke 2

--- text05.java
+++ text05.java
@@ -2,5 +2,6 @@ public class text05 {
 
     public static void main(String[] args) {
         System.out.println("hallo ini baris pertama");
+        System.out.println("Ini Rayhan, baris kode ke 2");
     }
 }
