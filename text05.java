176425d06ced90f9d3f0ef2b24c44684fc6c9c0a
anandarosyid <rosyidananda28@gmail.com>
2025-10-02 11:53:55 +0700

    Menambah baris ke 2

--- text05.java
+++ text05.java
@@ -2,5 +2,6 @@ public class text05 {
 
     public static void main(String[] args) {
         System.out.println("hallo ini baris pertama");
+        System.out.println("ini rosyid baris kedua");
     }
 }
